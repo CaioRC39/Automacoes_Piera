d86451e9d9ea194aeadf2cd84a3b3430f4881a30
CaioRC39 <168103569+CaioRC39@users.noreply.github.com>
2025-09-26 11:59:51 -0300

    Update Menu.py
    Foi mudado de "hub" para "menu";
    Adicionado nova indicação da nova ferramenta "Formatador_para_texto_NewPiit.py"

--- Automacoes/Menu.py
+++ Automacoes/Menu.py
@@ -1,5 +1,5 @@
 # ==============================================================================
-# HUB DE FERRAMENTAS DE AUTOMAÇÃO
+# MENU DE FERRAMENTAS DE AUTOMAÇÃO
 # ==============================================================================
 # Este é o arquivo principal (página de boas-vindas) do seu aplicativo
 # de múltiplas páginas no Streamlit.
@@ -8,22 +8,24 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hub de Ferramentas de Automação",
+    page_title="Menu de Ferramentas de Automação",
     page_icon="🤖",
     layout="wide"
 )
 
-st.title("Bem-vindo ao Hub de Ferramentas de Automação 👋")
+st.title("Bem-vindo ao Menu de Ferramentas de Automação da Piera! 👋")
 st.sidebar.success("Selecione uma ferramenta acima.")
 
 st.markdown(
     """
-    Este é um hub central para as ferramentas de automação de projetos de PD&I.
+    Este é um menu central para as ferramentas de automação de projetos de PD&I.
 
     **👈 Selecione uma ferramenta na barra lateral** para começar!
 
     ### Ferramentas Disponíveis:
-    - **Preenchedor de Planilha:** Preenche a planilha base a partir de múltiplos arquivos.
-    - **Gerador de Relatório:** Cria um relatório consolidado (LP, RH e ST) a partir da planilha de Valoração e dos TAs.
+    - **Preenchedor de NewPiit:** Preenche o NewPiit a partir de múltiplos arquivos.
+    - **Gerador de planilha com LP, Rh e ST:** Cria uma planilha consolidada com Linhas de Pesquisa, Recursos Humanos e Serviços de Terceiros e Viagens a partir da planilha de Valoração e dos TAs.
+    - **Formatador para texto de NewPiit:** Gera um texto formatado de uma aba selecionada do NewPiit, com direito a filtros.
     """
 )
+
